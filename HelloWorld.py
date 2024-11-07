b8c5cdb1ccf9a4cf4deddcb682ba1ded534b944a
jbblanton <jessicabblanton@gmail.com>
2024-11-07 14:56:53 -0500

    first commit

---
+++ HelloWorld.py
@@ -0,0 +1,21 @@
+# HelloWorld.py
+
+def hello_world():
+    print('Hello, World!')
+
+    body = {'message': 'Hello, World!'}
+
+    header = {}
+    header['Content-Type'] = 'application/json'
+    header['Access-Control-Allow-Headers'] = 'Content-Type'
+    header['Access-Control-Allow-Origin'] = '*'
+    header['Access-Control-Allow-Methods'] = 'OPTIONS,POST,GET'
+    #header['Access-Control-Allow-Credentials'] = 'true',
+    response = {
+        'isBase64Encoded': False,
+        'statusCode': 200,
+        'headers': header,
+        'body': body 
+    }
+
+    return response
