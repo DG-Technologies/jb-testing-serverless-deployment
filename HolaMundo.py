d9a5ef70a96dcc3d7618b262399b0d0ac6971e3f
jbblanton <jessicabblanton@gmail.com>
2024-11-07 15:15:01 -0500

    second commit

---
+++ HolaMundo.py
@@ -0,0 +1,21 @@
+# HolaMundo.py
+
+def hola_mundo():
+    print('Hola Mundo')
+
+    body = {'message': 'Hola Mundo'}
+
+    header = {}
+    header['Content-Type'] = 'application/json'
+    header['Access-Control-Allow-Headers'] = 'Content-Type'
+    header['Access-Control-Allow-Origin'] = '*'
+    header['Access-Control-Allow-Methods'] = 'OPTIONS,POST,GET'
+    #header['Access-Control-Allow-Credentials'] = 'true',
+    response = {
+        'isBase64Encoded': False,
+        'statusCode': 202,
+        'headers': header,
+        'body': body 
+    }
+
+    return response
